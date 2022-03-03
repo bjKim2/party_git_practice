013b1cf66ffd5daaf7ba44b483f199b812fe4c2a
essentials <coconutcacao@gmail.com>
2022-03-03 22:57:24 +0900

    -

--- Main.java
+++ Main.java
@@ -2,89 +2,100 @@ package com.javalec.base;
 
 import java.util.Scanner;
 
-import com.javalec.function.Addition;
-import com.javalec.function.Calc;
-import com.javalec.function.ConstCalc;
-import com.javalec.function.MethodCalc;
-
 public class Main {
 
 	public static void main(String[] args) {
-		//두 개의 정수를 입력 받아 덧셈 기능 구현하기
-		
-		Scanner scanner= new Scanner(System.in);
-		int num1 =0; //첫번째 수 
-		int num2 =0; //두번째 수
-		
-		System.out.println("첫 번째 수를 입력하세요.");
-		num1 = scanner.nextInt();
+		// TODO Auto-generated method stub
+
+//		Scanner scanner = new Scanner(System.in);
 		
 		
-		System.out.println("두 번째 수를 입력하세요.");
-		num2 = scanner.nextInt();
-		
-		//Field 
-		
-		ConstCalc calc = new ConstCalc(num1, num2);
-		System.out.println(num1 + "+" + num2 + "=" + calc.addition());
-		
-		System.out.println(num1 + "-" + num2 + "=" + calc.substraction());
-	
-		System.out.println(num1 + "*" + num2 + "=" + calc.multiplication());
-		
-		System.out.println(num1 + "/" + num2 + "=" + calc.division());
-		
-		
-//		MethodCalc calc = new MethodCalc();
-//		int addResult = calc.addition(num1,  num2);
-//		System.out.println(num1 + "+" + num2 + "=" + addResult);
+//		구구단 중에서 입력한 숫자의 단 출력
+//		System.out.print("원하는 숫자를 입력하세요:");
+//		int num = scanner.nextInt();
 //		
-//		int substractionResult = calc.substraction(num1,  num2);
-//		System.out.println(num1 + "-" + num2 + "=" + substractionResult);
+//		System.out.println("* 구구단 *");
+//		for (int i=1 ; i<=9 ; i++) {
+//			System.out.println(num + " x " + i + " = " + (num*i));
+//		}
 //		
-//		int multiplicationResult = calc.multiplication(num1,  num2);
-//		System.out.println(num1 + "*" + num2 + "=" + multiplicationResult);
 //		
-//		double divisionResult = calc.division(num1, num2);
-//		System.out.println(num1 + "/" + num2 + "=" + divisionResult);
+
+		// 2단부터 9단까지
+//		for (int dan=2 ; dan<=9 ; dan++) {
+//			System.out.println("** " + dan + " 단 **");
+//			for (int i=1 ;i<=9 ; i++) {
+//				System.out.println(dan + " x " + i + " = " + (dan * i));
+//			}
+//			System.out.println("-----------");
+//		
+//	}
+
 		
 		
-//		Calc calc = new Calc();
-//		calc.num1 = num1;
-//		calc.num2 = num2;
+//	for (int i = 1 ; i<=9 ; i++) {
+//		for(int dan = 2; dan <=9; dan++) {
+//			System.out.print(dan + " X " + i + " = " + (dan * i) + "\t");
+//		}
+//		System.out.println();
+//		}
+
 		
-//		calc.addition();
-//		calc.substraction();
-//		calc.multiplication();
-//		calc.division();
-		
-//		calc.calcAll();
-//		System.out.println(calc.addition());
+		//짝수단에 곱하는 수가 짝수인 경우만 출력
+//	for (int i = 1 ; i<=9 ; i++) {
 //	
-//		System.out.println(calc.multiplication());
-//	
+//		for(int dan = 2; dan <=9; dan++) {
+//			if (i % 2 == 0 && dan % 2 == 0 ) {
+//			System.out.print(dan + " X " + i + " = " + (dan * i) + "\t");
+//		}
+//		}
+//		System.out.println();
+//	}
+
+		//답이 홀수인 경우 자리 비워놓기
+//		for (int i = 1; i <= 9; i++) {
+//
+//			for (int dan = 2; dan <= 9; dan++) {
+//				if ((dan * i) % 2 == 0) {
+//					System.out.print(dan + " X " + i + " = " + (dan * i) + "\t");
+//				} else {
+//					System.out.print("\t\t");
+//				}
+//			}
+//			System.out.println();
+//
+//		}
 		
 		
-				
-		//덧셈 기능 구현하기
-		//Case 1: Field 를 이용하여 계산하기
+		//홀수가 답인 부분을 언더바로 표시
 		
-//		Addition addition = new Addition(); //Addtion 타이핑할 때, Ctrl +space
-//		addition.num1 = num1;
-//		addition.num2 = num2;
-//		addition.addAction();
-//		addition.subtractionAction();
-//		addition.multiplicationAction();
-//		addition.divisionAction();
-//		
-//		
-//	
-//		System.out.println(num1 + "+ " +num2 + "=" + addition.addReturn());
-//		System.out.println(num1 + "- " +num2 + "=" + addition.subtractionReturn());
-//		System.out.println(num1 + "* " +num2 + "=" + addition.multiplicationReturn());
-//		System.out.println(num1 + "/ " +num2 + "=" + addition.divisionReturn());
-//		
+//		for (int i = 1; i <= 9; i++) {
+//			
+//			for (int dan = 2; dan <= 9; dan++) {
+//				if ((dan * i) % 2 == 0) {
+//					System.out.print(dan + " X " + i + " = " + (dan * i) + "\t");
+//				} else {
+//					System.out.print(dan + " X " + i + " = " + "__" + "\t");
+//				}
+//			}
+//			System.out.println();
+//			
+//		}
 		
+		
+		//곱하는 수가 홀수인 경우 *로 표시하기.
+		for (int i = 1; i <= 9; i++) {
+			
+			for (int dan = 2; dan <= 9; dan++) {
+				if (i % 2 == 0) {
+					System.out.print(dan + " X " + i + " = " + (dan * i) + "\t");
+				} else {
+					System.out.print(dan + " X " + "*" + " = " + (dan * i) + "\t");
+				}
+			}
+			System.out.println();
+			
+		}
 	}
 
 }
