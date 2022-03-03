2d47b760fd34ac2e32a8fcfb49c24c38b7aefb4d
essentials <coconutcacao@gmail.com>
2022-03-03 18:35:43 +0900

    Create MethodCalc.java

---
+++ MethodCalc.java
@@ -0,0 +1,29 @@
+package com.javalec.function;
+
+public class MethodCalc {
+
+	// Field
+//	int num1;
+//	int num2;
+
+	// Constructor
+	public MethodCalc() {
+		// TODO Auto-generated constructor stub
+	}
+
+	// Method
+	public int addition(int num1, int num2) {
+		return (num1 + num2);
+	}
+	
+	public int substraction(int num1, int num2) {
+		return (num1 - num2);
+	}
+	public int multiplication(int num1, int num2) {
+		return (num1 * num2);
+	}
+				
+	public double division(int num1, int num2) {
+		return (num1 /(double)num2);
+	}
+}
